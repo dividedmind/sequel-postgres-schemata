6b3aebf4d2bdc44fb8e60d1e2e8d3a4b06eb8fcd
Rafał Rzepecki <divided.mind@gmail.com>
2013-10-05 02:55:45 +0200

    search_path, schemata

--- lib/sequel/postgres/schemata.rb
+++ lib/sequel/postgres/schemata.rb
@@ -2,7 +2,31 @@ require "sequel/postgres/schemata/version"
 
 module Sequel
   module Postgres
-    module Schemata        
+    module Schemata
+      module DatabaseMethods
+
+        # List all existing schematas (including the system ones).
+        # Returns a symbol list.
+        def schemata
+          metadata_dataset.select(:nspname).from(:pg_namespace).map(:nspname).map(&:to_sym)
+        end
+        
+        # Returns a symbol list containing the current search path.
+        # Note that the search path can contain non-existent schematas.
+        def search_path
+          metadata_dataset.with_sql(SHOW_SEARCH_PATH).
+            single_value.gsub('""', '"').split(", ").map(&:to_sym)
+        end
+        
+        private
+        
+        SHOW_SEARCH_PATH = "SHOW search_path".freeze
+
+      end
+    end
+    
+    module DatabaseMethods
+      include ::Sequel::Postgres::Schemata::DatabaseMethods
     end
   end
 end
--- spec/schemata_spec.rb
+++ spec/schemata_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 describe Sequel::Postgres::Schemata do
+  
+  let(:db) { Sequel::connect adapter: 'postgres', search_path: %w(foo public) } 
 
   describe "#schemata" do
-    it "lists all existing schematas"
+    it "lists all existing schematas" do
+      schemata = db.schemata
+      schemata.should include(:public)
+      schemata.should_not include(:foo)
+    end
   end
   
   describe "#search_path" do
-    it "returns the search path"
+    it "returns the search path" do
+      db.search_path.should == %i(foo public)
+    end
   end
   
   describe "#search_path=" do
